33f8e83c7ab8ec233be9b4abe64033f75b9224ff
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-06-23 23:41:39 +0200

    To fix

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttoClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogPageComponent } from './blog-page/blog-page.component';
@@ -11,4 +11,30 @@ import { ArticleEditComponent } from './article-edit/article-edit.component';
 import { PostBlockComponent } from './post-block/post-block.component';
 import { PaginationComponent } from './pagination/pagination.component';
 import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BlogRoutingModule,
+    FormsModule, ReactiveFormsModule,
+    MarkdownModule.forRoot({
+      loader: HttpClientModule, // optional, only if you use [src] attribute
+      markedOptions: {
+        provide: MarkedOptions,
+        useValue: {
+          gfm: true,
+          tables: true,
+          breaks: false,
+          pedantic: false,
+          sanitize: false,
+          smartLists: true,
+          smartypants: false,
+        },
+      },
+    })
+  ],
+  declarations: [BlogPageComponent, ArticleComponent, ArticleCreateComponent, ArticleEditComponent, PostBlockComponent, PaginationComponent]
+})
+
+export class BlogModule { }
 
