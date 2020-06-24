aa423095a372eb0069f11e2a67a8a177485dcbbb
vscalcione <vincenzo99.scalcione@gmail.com>
2020-06-24 22:09:31 +0200

    Fixing code in article, article-create, article-edit, blog-page components

--- src/app/blog/article-edit/article-edit.component.ts
+++ src/app/blog/article-edit/article-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConfigService } from '../../config.service';
+import { Location } from '@angular/common';
+import {AuthenticationService} from '../../autentication.service';
 
 @Component({
   selector: 'app-article-edit',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleEditComponent implements OnInit {
 
-  constructor() { }
+  postEditForm: FormGroup;
+  id: number;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+              private auth: AuthenticationService,
+              private config: ConfigService,
+              private route: ActivatedRoute,
+              private location: Location,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.postEditForm = this.fb.group({
+      'id' : [null, Validators.required],
+      'title' : [null, Validators.required],
+      'author' : [null, Validators.required],
+      'publishdate' : [null, Validators.required],
+      'excert' : [null, Validators.required],
+      'image' : [null, Validators.required],
+    });
+    this.id = this.route.snapshot.params['id'] || null ;
+    if (this.id) {
+      this.getPostById(this.id);
+    }
   }
 
+  getPostById(id: number) {
+    this.config.getPostByID(id).subscribe(
+      post => {
+        this.postEditForm.setValue({
+          id: post.id,
+          title: post.title,
+          author: post.author,
+          publishdate: post.publishdate,
+          excert: post.excert,
+          image: post.image
+        });
+      }
+    );
+  }
+
+  updatePost(formData: NgForm) {
+    this.config.updatePost(formData).subscribe(
+      () => this.getBack()
+    );
+  }
+
+  getBack() {
+    this.location.back();
+  }
 }
--- src/app/blog/blog-page/blog-page.component.ts
+++ src/app/blog/blog-page/blog-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../../config.service';
+import { Post } from '../post';
+import { PagerService } from '../pager.service';
 
 @Component({
   selector: 'app-blog-page',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPageComponent implements OnInit {
 
-  constructor() { }
+  blog: { id: string,
+    tagline: string,
+    title: string,
+    description: string,
+  }[];
 
-  ngOnInit(): void {
+  allItems: any[];
+  pages: any[];
+  pageSize = 3;
+  pager: any = {};
+  posts: Post[];
+
+  constructor(private config: ConfigService, private pagerService: PagerService) { }
+
+  ngOnInit() {
+    this.getPageData('pages', 'blog');
+    this.getBlockData('posts');
+  }
+
+  getPageData(database: string, id?: string) {
+    this.config.getSettings(database, id).subscribe(
+      data => {
+        this.blog = data;
+        console.log(this.blog);
+      }
+    );
+  }
+
+  getBlockData(database: string ) {
+    this.config.getSettings(database ).subscribe(
+      data => {
+        this.posts = data;
+        this.allItems = this.posts;
+        this.setPage(1);
+        console.log(this.posts);
+      }
+    );
+  }
+
+  setPage(pageNumber: number) {
+    // create a pager using paging service
+    this.pager = this.pagerService.getPager(this.allItems.length, pageNumber, this.pageSize);
+
+    // current page posts
+    this.pages = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    console.log(this.pager);
   }
 
 }
